a9e37322ecad4695d01e6d741207dc3725ba4a56
Gilbert Kwan <gkwan@ca.ibm.com>
2022-04-05 15:38:04 -0400

    Delete finish/scripts directory (#4)
    * Delete finish/scripts directory
    
    * Update dailyBuild.sh
    
    * Update README.adoc
    
    * Update README.adoc

--- finish/scripts/buildImages.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker pull openliberty/open-liberty:kernel-java8-openj9-ubi
-oc process -f build.yaml -p APP_NAME=system | oc create -f -
-oc start-build system-buildconfig --from-dir=system/.
--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -3,8 +3,6 @@ while getopts t:d:b:u: flag; do
     case "${flag}" in
     t) DATE="${OPTARG}" ;;
     d) DRIVER="${OPTARG}" ;;
-    b) BUILD="${OPTARG}" ;;
-    u) DOCKER_USERNAME="${OPTARG}" ;;
     *) echo "Invalid option" ;;
     esac
 done
@@ -13,10 +11,5 @@ echo "Testing daily build image"
 
 sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" system/pom.xml
 cat system/pom.xml
-
-sed -i "s;FROM openliberty/open-liberty:full-java11-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD;g" system/Dockerfile
-cat system/Dockerfile
-
-docker pull "$DOCKER_USERNAME/olguides:$BUILD"
 
 sudo ../scripts/testApp.sh
